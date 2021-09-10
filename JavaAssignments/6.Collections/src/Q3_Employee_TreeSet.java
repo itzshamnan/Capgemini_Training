c0fc9a503bf89935c3cc11ee778952af55a83983
itzshamnan <mohamedshamnan@gmail.com>
2021-09-10 22:46:07 +0530

    Committing

--- JavaAssignments/6.Collections/src/Q3_Employee_TreeSet.java
+++ JavaAssignments/6.Collections/src/Q3_Employee_TreeSet.java
@@ -2,95 +2,116 @@ import java.util.Comparator;
 import java.util.Scanner;
 import java.util.TreeSet;
 
-class Employee {
+class employee{
+    int id;
+    String name;
+    int salary;
+    String Department;
 
-    public Integer id;
-    public String name;
-    public Double salary;
-    public String department;
-    public Employee(Integer id, String name, Double salary, String department) {
-        super();
+    public employee(int id, String name, int salary, String department) {
         this.id = id;
         this.name = name;
         this.salary = salary;
-        this.department = department;
-    }
-    public void setId(Integer id) {
-        this.id = id;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public void setSalary(Double salary) {
-        this.salary = salary;
-    }
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-
-    public Integer getId() {
-        return id;
-    }
-    public String getName() {
-        return name;
-    }
-    public Double getSalary() {
-        return salary;
-    }
-    public String getDepartment() {
-        return department;
-    }
-    @Override
-    public String toString() {
-
-        return "Employee [id =" + this.id + ", name = " +this.name +", salary= "+ this.salary +", department= "+ this.department+"]";
-    }
-    public static void options(){
-        System.out.println("a) ID");
-        System.out.println("b) Name");
-        System.out.println("a) Salary");
-        System.out.println("a) Department");
-        System.out.println("Enter your choice");
-        Scanner scan=new Scanner(System.in);
-        char opt=scan.next().charAt(0);
-
-
-        }
-
-
-    }
-
-
-public class Q3_Employee_TreeSet {
-    public static void main(String[] args) {
-
-
-        TreeSet<Employee> obj=new TreeSet<>(new FirstComparator());
-        obj.add(new Employee(1,"ABCD",41000.0,"A4"));
-        obj.add(new Employee(2,"XYZ",22000.0,"AA2"));
-        obj.add(new Employee(3,"EFGH",63000.0,"C8"));
-        obj.add(new Employee(4,"PQRST",56666.0,"CC8"));
-        obj.add(new Employee(5,"UVW",200000.0,"NM4"));
-        obj.add(new Employee(6,"HIJK",77000.0,"T9"));
-        obj.add(new Employee(7,"LMNO",8222.0,"A5"));
-        Employee.options();
-        for(Employee e :obj) {
-            System.out.println(e);
-        }
-
-
-
-
-
-
+        Department = department;
     }
 }
-class FirstComparator implements Comparator<Employee> {
-    @Override public int compare(Employee e1, Employee e2)
+
+class NameComparator implements Comparator<employee>
+{
+
+    public int compare(employee e1, employee e2)
     {
-        return (e1.id).compareTo(e2.id);
-
-
+        return e1.name.compareTo(e2.name);
     }
+}
+class SalaryComparator implements Comparator<employee>
+{
+
+    public int compare(employee e1, employee e2)
+    {
+        return e1.salary-e2.salary;
+    }
+}
+class DeptComparator implements Comparator<employee>
+{
+
+    public int compare(employee e1, employee e2)
+    {
+        return e1.Department.compareTo(e2.Department);
+    }
+}
+class IdComparator implements Comparator<employee>
+{
+
+    public int compare(employee e1, employee e2)
+    {
+        return e1.id-e2.id;
+    }
+}
+public class Q3_Employee_TreeSet {
+
+    public static void main(String[] args) {
+
+        int ch;
+        Scanner in = new Scanner(System.in);
+        char f = 'y';
+        while (f=='y'){
+
+            System.out.println("1) ID");
+            System.out.println("2) Name");
+            System.out.println("3) Salary");
+            System.out.println("4) Department");
+            System.out.println("Enter your choice");
+
+
+            ch = in.nextInt();
+            switch (ch){
+                case 2:
+                    TreeSet<employee>employeeTreeSet = new TreeSet<>(new NameComparator());
+                    AddToTree(employeeTreeSet);
+                    DisplayTree(employeeTreeSet);
+                    break;
+                case 3:
+                    TreeSet<employee>employeeTreeSet1 = new TreeSet<>(new SalaryComparator());
+                    AddToTree(employeeTreeSet1);
+                    DisplayTree(employeeTreeSet1);
+                    break;
+                case 4:
+                    TreeSet<employee>employeeTreeSet2 = new TreeSet<>(new DeptComparator());
+                    AddToTree(employeeTreeSet2);
+                    DisplayTree(employeeTreeSet2);
+                    break;
+                case 1:
+                    TreeSet<employee>employeeTreeSet3 = new TreeSet<>(new IdComparator());
+                    AddToTree(employeeTreeSet3);
+                    DisplayTree(employeeTreeSet3);
+                    break;
+                default:
+                    System.out.println("invalid choice");
+                    break;
+
+            }
+            System.out.println("\ndo you want to continue? y/n");
+            f=in.next().charAt(0);
+        }
+    }
+
+    private static void DisplayTree(TreeSet<employee> employeeTreeSet) {
+
+        for (employee ob:employeeTreeSet)
+        {
+            System.out.print(ob.id+" "+ob.name+" "+ ob.salary+" "+ob.Department+"\n");
+        }
+    }
+
+    private static void AddToTree(TreeSet<employee> employeeTreeSet) {
+        employeeTreeSet.add(new employee(1,"ABCD",41000,"A4"));
+        employeeTreeSet.add(new employee(2,"XYZ",22000,"AA2"));
+        employeeTreeSet.add(new employee(3,"EFGH",63000,"C8"));
+        employeeTreeSet.add(new employee(4,"PQRST",56666,"CC8"));
+        employeeTreeSet.add(new employee(5,"UVW",200000,"NM4"));
+        employeeTreeSet.add(new employee(6,"HIJK",77000,"T9"));
+        employeeTreeSet.add(new employee(7,"LMNO",8222,"A5"));
+    }
+
 }
--- JavaAssignments/6.Collections/src/q4_LinkedList.java
+++ JavaAssignments/6.Collections/src/q4_LinkedList.java
@@ -1,9 +1,25 @@
-
+import java.time.LocalDate;
+import java.util.LinkedList;
 
 public class q4_LinkedList {
     public static void main(String[] args) {
 
+LinkedList<LocalDate> BirthDates = new LinkedList<>();
+        BirthDates.add(LocalDate.of(2000,8,3));
+        BirthDates.add(LocalDate.of(1998,4,27));
+        BirthDates.add(LocalDate.of(2012,6,4));
+        BirthDates.add(LocalDate.of(2007,1,10));
+        BirthDates.add(LocalDate.of(2020,3,21));
+        //System.out.println(BirthDates);
+        for (LocalDate d:BirthDates)
+        {
+            if(d.isLeapYear())
+                System.out.println(d+" is a Leap year. \n");
+            else
+                System.out.println(d+" is not a Leap year.");
+
+        }
 
 
-    }
+}
 }
