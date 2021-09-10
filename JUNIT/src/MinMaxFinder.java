64b9b03003bf2c2db88b33c820b7df97e5143fab
itzshamnan <mohamedshamnan@gmail.com>
2021-09-10 12:49:17 +0530

    Committing

--- JUNIT/src/MinMaxFinder.java
+++ JUNIT/src/MinMaxFinder.java
@@ -1,29 +1,19 @@
 import java.util.Arrays;
-class result{                       //updation for question2
-    int min;
-    int max;
 
-    public result(int min, int max) {
-        this.min = min;
-        this.max = max;
-    }
-}
-class MinMaxFinder{
-
-    public result FindMinMax(int[] a)
-    {
-        if (a.length==0)
+class MinMaxFinder {
+    public int[] FindMinMax(int[] a) {
+        if (a.length == 0)
             throw new ArrayStoreException("Array size is zero");
 
-        int [] result = new int[10];
+        int[] result = new int[10];
         Arrays.sort(a);
-        /*result[0]=a[0];                     //question1 code
-        result[1]=a[a.length-1];*/
+        result[0] = a[0];                     //question1 code
+        result[1] = a[a.length - 1];
 
-        /*if (result[0]==result[1])
-            throw new ArrayStoreException("all array elements are same..!!");*/
-        result r = new result(a[0],a[a.length-1]);
-        return r;
+        if (result[0] == result[1])
+            throw new ArrayStoreException("all array elements are same..!!");
 
+        return result;
     }
+
 }
